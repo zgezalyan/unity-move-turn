9a5a1b25343a3124c0b8aee5ce2d9ff880d137eb
Zurab Gezalyan <mcdoss@mail.ru>
2024-07-15 13:09:46 +0300

    Task complete

---
+++ Assets/Scripts/PlaneControl.cs
@@ -0,0 +1,76 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Random = System.Random;
+
+public class PlaneControl : MonoBehaviour
+{
+    public int _numberOfPoints;
+    public int _speed;
+
+    private Vector3[] pointsArray;
+    private Vector3 currentDestination;
+    private int currentDestinationIndex;
+    private bool isForward;
+
+    public void Start()
+    {        
+        GeneratePointArray();
+        isForward = true;
+        SetNextDestination(0);
+        transform.LookAt(currentDestination); 
+    }
+
+    
+    public void Update()
+    {
+        if (transform.position != currentDestination)
+            transform.position = Vector3.MoveTowards(transform.position, currentDestination, Time.deltaTime * _speed);
+        else
+        {
+            if (isForward)
+            {
+                if (currentDestinationIndex < _numberOfPoints - 1)
+                    SetNextDestination(currentDestinationIndex + 1);
+                else
+                {
+                    SetNextDestination(currentDestinationIndex - 1);
+                    isForward = false;
+                }
+            }
+            else
+            {
+                if (currentDestinationIndex > 0)
+                    SetNextDestination(currentDestinationIndex - 1);
+                else
+                {
+                    SetNextDestination(currentDestinationIndex + 1);
+                    isForward = true;
+                }
+            }
+            transform.LookAt(currentDestination);
+        }
+        transform.Rotate(0, 0, 2);
+    }
+
+    public void GeneratePointArray()
+    {
+        pointsArray = new Vector3[_numberOfPoints];
+        Random rnd = new Random();
+
+        for (int i = 0; i < _numberOfPoints; i++)
+        {
+            float newX = rnd.Next(-1001, 1001) * 0.01f;
+            float newY = rnd.Next(100, 701) * 0.01f;
+            float newZ = rnd.Next(-1001, 1001) * 0.01f;
+
+            pointsArray[i].Set(newX, newY, newZ);
+        }        
+    }
+
+    public void SetNextDestination(int index)
+    {
+        currentDestinationIndex = index;
+        currentDestination = pointsArray[currentDestinationIndex];
+    }
+}
---
+++ Assets/Scripts/RunnersControl.cs
@@ -0,0 +1,51 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RunnersControl : MonoBehaviour
+{
+
+    public GameObject _runner1;
+    public GameObject _runner2;
+    public GameObject _runner3;
+    public GameObject _runner4;
+    public GameObject _runner5;
+    public GameObject _runner6;
+    public GameObject _runner7;
+    public GameObject _runner8;
+    public GameObject _runner9;
+    public GameObject _runner10;
+    public float _transferSpace;
+    public float _speed;
+    
+    private GameObject[] runnersArray;
+    private int currentRunnerIndex;
+    private int nextRunnerIndex;    
+
+    void Start()
+    {        
+        currentRunnerIndex = 0;
+        nextRunnerIndex = currentRunnerIndex + 1;        
+        runnersArray = new GameObject[10] { _runner1, _runner2, _runner3, _runner4, _runner5, _runner6, _runner7, _runner8, _runner9, _runner10 };
+        runnersArray[currentRunnerIndex].transform.LookAt(runnersArray[nextRunnerIndex].transform);
+        runnersArray[nextRunnerIndex].transform.LookAt(runnersArray[currentRunnerIndex].transform);
+    }
+    
+    void Update()
+    {        
+        if (Vector3.Distance(runnersArray[currentRunnerIndex].transform.position, runnersArray[nextRunnerIndex].transform.position) > _transferSpace)
+            runnersArray[currentRunnerIndex].transform.position = Vector3.MoveTowards(runnersArray[currentRunnerIndex].transform.position, runnersArray[nextRunnerIndex].transform.position, Time.deltaTime * _speed);
+        else
+        {
+            currentRunnerIndex = nextRunnerIndex;
+            if (currentRunnerIndex == 9)
+                nextRunnerIndex = 0;
+            else
+                nextRunnerIndex++;
+            runnersArray[currentRunnerIndex].transform.LookAt(runnersArray[nextRunnerIndex].transform);
+            runnersArray[nextRunnerIndex].transform.LookAt(runnersArray[currentRunnerIndex].transform);
+        }
+        
+        transform.position = runnersArray[currentRunnerIndex].transform.position + runnersArray[currentRunnerIndex].transform.TransformDirection(new Vector3 (0.06f, 0, 0.16f));
+    }
+}
